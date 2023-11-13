83bd9119afa97339de07736b11eb480bedf04d7c
xpazv <xavier@tresipunt.com>
2023-11-13 14:54:41 +0100

    INTERACT-157: update vimeo using composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit256a73115b96722060505506a2185113
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
