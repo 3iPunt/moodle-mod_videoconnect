aed7c199a5acb149715f09f309b1d896ed3648ca
Antoni Bertran <antoni@tresipunt.com>
2024-09-18 10:44:41 +0200

    Check current context page to avoid warnings

--- lib.php
+++ lib.php
@@ -78,7 +78,9 @@ function videoconnect_add_instance(object $moduleinstance, $mform = null): int {
  */
 function videoconnect_get_coursemodule_info(object $coursemodule): cached_cm_info {
     global $PAGE;
-    $PAGE->set_context(context_module::instance($coursemodule->id));
+    if ($PAGE->context->id != context_module::instance($coursemodule->id)) {
+        $PAGE->set_context(context_module::instance($coursemodule->id));
+    }
     $output = $PAGE->get_renderer('mod_videoconnect');
     $page = new view_page($coursemodule->id, false);
     $content = $output->render($page);
